bdbb888b24163fc5a879c12743c897434bba1bfc
Imran <strongsoda2@gmail.com>
2018-09-06 20:17:53 +0530

    bug fix

--- index.js
+++ index.js
@@ -50,3 +50,29 @@ app.get('/auth/facebook/callback',
   function(req, res) {
     res.redirect('/success');
   });
+
+  /*  GITHUB AUTH  */
+
+const GitHubStrategy = require('passport-github').Strategy;
+
+const GITHUB_CLIENT_ID = "283749a55df87e515f68"
+const GITHUB_CLIENT_SECRET = "d15b3a66f47ac79f8fe0eb2bd585499f7a9b0aab";
+
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      return cb(null, profile);
+  }
+));
+
+app.get('/auth/github/',
+  passport.authenticate('github'));
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/error' }),
+  function(req, res) {
+    res.redirect('/success');
+  });
